d5fc2167d0900b23cee00b70a94cf0fbd38cdb23
javaughn-bradford <javaughn.bradford001@albright.edu>
2025-04-10 10:56:56 -0400

    Create week13easyAssignment.cpp

---
+++ week13/week13easyAssignment.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+	std::cout<< "Countdown until liftoff: \n";
+	for (int i=10; i>0; i--){
+		std::cout<< i << ". . . \n";
+	}
+	std::cout<< "Liftoff!!!!"<<"\n";
+return 0;
+}

de682a95d9bb300b3149a65669e921d88324374b
javaughn-bradford <javaughn.bradford001@albright.edu>
2025-04-14 11:21:20 -0400

    Create easy_jump_into_hyperspace.cpp

---
+++ week14/easy_jump_into_hyperspace.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+	std::cout<< "Get ready for hyperspace chewie! \n";
+	for (int i=5; i>0; i--){
+		std::cout<<"Jumping in " << i << ". . . \n";
+	}
+	std::cout<< "Punch it R2D2! "<<"\n";
+return 0;
+}

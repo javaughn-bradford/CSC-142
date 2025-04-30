7ad101ac202f82e3d5966188c6a330c42d0b2494
javaughn-bradford <javaughn.bradford001@albright.edu>
2025-04-30 09:27:07 -0400

    Create batmaneasy.cpp

---
+++ week15/batmaneasy.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::string name = "Joker";
+    std::string location = "Arkham Asylum";
+
+    // Output the information to the console
+    std::cout << name << " spotted at " << location << "\n";
+
+    return 0;
+}
